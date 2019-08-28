2a611c833b6a2f9ada017806b9a94fdf3b6ea6f5
Luigi Klimuk <luigiklimuk@Luigis-MacBook-Pro.local>
2019-08-28 17:03:13 -0300

    header

--- ferr-laravel/resources/views/home.blade.php
+++ ferr-laravel/resources/views/home.blade.php
@@ -1,25 +1,8 @@
-@extends('layouts.app')
+@extends('layouts.master')
+
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-
-            </div>
-        </div>
-    </div> -->
 
     <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
@@ -88,6 +71,10 @@
 
             </div>
         </div>
+        <main class="py-4">
+            @yield('content')
+        </main>
     </section>
+    
 </div>
 @endsection
