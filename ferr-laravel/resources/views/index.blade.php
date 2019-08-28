1e41e9d0f15613ef6192c412451b40bc23a797ac
fronthendy <hendy.almeida@hotmail.com>
2019-08-27 22:07:33 -0300

    fix front

--- ferr-laravel/resources/views/index.blade.php
+++ ferr-laravel/resources/views/index.blade.php
@@ -1,57 +1,80 @@
 @extends('layouts.master')
 
 @section('title')
-    Inicio
+Inicio
 @endsection
 
 
 @section('conteudo')
-   <h1>PRINCIPAIS PRODUTOS</h1>
+<div class="container">
 
-   <p>
-    <a class="btn btn-primary" data-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Carpintaria</a>
-    <a class="btn btn-primary" data-toggle="collapse" href="#multiCollapseExample2" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Demolição</a>
-    <a class="btn btn-primary" data-toggle="collapse" href="#multiCollapseExample" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Jardinagem</a>
-    <a class="btn btn-primary" data-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Limpeza</a>
-    <a class="btn btn-primary" data-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Cortes</a>
-    </p>
-    <div class="row">
-        <div class="col">
-            <div class="collapse multi-collapse" id="multiCollapseExample1">
-                <div class="card card-body">
-                    Anim pariatur cliche reprehenderit, enim alemanha 7x1 brasil life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
-                </div>
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="images/banner1.jpg" alt="Primeiro Slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="images/banner2.jpg" alt="Segundo Slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="images/banner3.jpg" alt="Terceiro Slide">
             </div>
         </div>
-    
-        <div class="col">
-            <div class="collapse multi-collapse" id="multiCollapseExample2">
-                <div class="card card-body">
-                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
-                </div>
-            </div>
-        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Anterior</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Próximo</span>
+        </a>
     </div>
 
-   <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img class="d-block w-100" src="images/banner1.jpg" alt="Primeiro Slide">
-        </div>
-        <div class="carousel-item">
-            <img class="d-block w-100" src="images/banner2.jpg" alt="Segundo Slide">
-        </div>
-        <div class="carousel-item">
-            <img class="d-block w-100" src="images/banner3.jpg" alt="Terceiro Slide">
-        </div>
+    <div class="categorias d-flex ">
+        <a class="btn btn-primary p-2 flex-fill" href="/produtos/categoria/id">Carpintaria</a>
+        <a class="btn btn-primary p-2 flex-fill" href="/produtos/categoria/id">Demolição</a>
+        <a class="btn btn-primary p-2 flex-fill" href="/produtos/categoria/id">Jardinagem</a>
+        <a class="btn btn-primary p-2 flex-fill" href="/produtos/categoria/id">Limpeza</a>
+        <a class="btn btn-primary p-2 flex-fill" href="/produtos/categoria/id">Cortes</a>
     </div>
-    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Anterior</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Próximo</span>
-    </a>
+    <h1 class="mt-5">PRINCIPAIS PRODUTOS</h1>
+    <section class="produtos d-flex flex-wrap justify-content-between mb-5">
+        <div class="card mt-3">
+            <div class="card-body">
+                <a href="/produtos/id"><img src="/images/andaime2.jpg" class="img-thumbnail" alt="nome produto"></a>
+
+                <h5 class="card-title mt-2">Nome Produto</h5>
+                <h4>R$30.00</h4>
+
+            </div>
+        </div>
+        <div class="card mt-3">
+            <div class="card-body">
+                <a href="/produtos/id"><img src="/images/andaime2.jpg" class="img-thumbnail" alt="nome produto"></a>
+
+                <h5 class="card-title mt-2">Nome Produto</h5>
+                <h4>R$30.00</h4>
+
+            </div>
+        </div>
+        <div class="card mt-3">
+            <div class="card-body">
+                <a href="/produtos/id"><img src="/images/andaime2.jpg" class="img-thumbnail" alt="nome produto"></a>
+
+                <h5 class="card-title mt-2">Nome Produto</h5>
+                <h4>R$30.00</h4>
+
+            </div>
+        </div>
+        <div class="card mt-3">
+            <div class="card-body">
+                <a href="/produtos/id"><img src="/images/andaime2.jpg" class="img-thumbnail" alt="nome produto"></a>
+
+                <h5 class="card-title mt-2">Nome Produto</h5>
+                <h4>R$30.00</h4>
+
+            </div>
+        </div>
+    </section>
 </div>
 @endsection
