7de34cf9ca564c3a428088230d9d5c0207df0a21
csouza0211 <carlos.asouza@outlook.com.br>
2019-08-28 17:00:55 -0300

    Atualização da Home.Blade.PHP

--- ferr-laravel/resources/views/home.blade.php
+++ ferr-laravel/resources/views/home.blade.php
@@ -1,8 +1,8 @@
-@extends('layouts.app')
+@extends('layouts.master')
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <!-- <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
